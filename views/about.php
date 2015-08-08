1bbb0e2f2d9f7f7cbe68d3ae934185ea4a3d7f13
Abdi Mahamed <abdimahamed0987@gmail.com>
2015-08-08 10:27:55 +0000

    Dir change

--- views/about.php
+++
@@ -1,5 +0,0 @@
-<div class="starter-template">
-    <h1>About page</h1>
-    <p class="lead">This is the about page loaded with ViaJS
-        <br>Click other links to load other content</p>
-</div>
--- views/index.php
+++
@@ -1,5 +0,0 @@
-<div class="starter-template">
-    <h1>Content</h1>
-    <p class="lead">This page is inside the container
-        <br>it will be affected and populated with new elements</p>
-</div>
