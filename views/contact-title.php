acef0307dd1536238acf1fc6fd47ce490b471569
Iskren.Stanislavov <iskren.jgd@gmail.com>
2016-01-21 15:10:23 +0200

    load more than one piece of html into a single page
    var views = {
    home: [{
    selector: "#title",
    templateUrl: 'views/index-title.php'
    }, {
    selector: "#content",
    templateUrl: 'views/index-content.php'
    },
    ],
    about: [{
    selector: "#title",
    templateUrl: 'views/about-title.php'
    }, {
    selector: "#content",
    templateUrl: 'views/about-content.php'
    },
    ],
    contact: [{
    selector: "#title",
    templateUrl: 'views/contact-title.php'
    // }, {
    //     selector: "#content",
    //     templateUrl: 'views/contact-content.php'
    },
    ],
    defaultView: {
    view: 'home'
    }
    };

---
+++ views/contact-title.php
@@ -0,0 +1,5 @@
+<div class="starter-template">
+    <h1>Contact Page</h1>
+    <p class="lead">this page would not have content part
+        <br>but you can select some other page to check the functionality</p>
+</div>
--- views/index-content.php
+++ views/index-content.php
@@ -1,5 +1,5 @@
 <div class="starter-template">
-    <h1>Content</h1>
+    <h1>HOME PAGE Content</h1>
     <p class="lead">This page is inside the container
         <br>it will be affected and populated with new elements</p>
 </div>
